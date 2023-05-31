b645fa27d444a81d2eed4eaf02fb620a7af9a87b
Arif Iqbal <arif_iqbal@yahoo.com>
2023-05-31 14:26:22 -0600

    modified:   index.html modified:   script.js modified:   style.css

--- script.js
+++ script.js
@@ -1,6 +1,35 @@
 const scrollContainer = document.querySelector('main');
+console.log(scrollContainer);
 
+const endWidth = 25;
 scrollContainer.addEventListener('wheel', (evt) => {
+  // console.log(evt.target.window);
+  console.log((scrollContainer.scrollLeft += evt.deltaY));
   evt.preventDefault();
-  scrollContainer.scrollLeft += evt.deltaY;
+  let scrolled = (scrollContainer.scrollLeft += evt.deltaY);
+  console.log(scrollContainer.scrollLeft);
+  const sectionWidth = scrollContainer.scrollWidth;
+  const totalWidth = (scrollContainer.scrollLeft / sectionWidth) * 100;
+  console.log(totalWidth);
+  document.getElementById('myBar').style.width = totalWidth + endWidth + '%';
 });
+
+/***********W3****************************************** 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  console.log('I am scrolled');
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  console.log(winScroll);
+  var height =
+  document.documentElement.scrollHeight -
+  document.documentElement.clientHeight;
+  console.log(height);
+  var scrolled = (winScroll / height) * 100;
+  console.log(scrolled);
+  document.getElementById('myBar').style.width = scrolled + '%';
+}
+/***********W3****************************************** */
