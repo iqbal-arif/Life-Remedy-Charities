840588dc759769a75c62a0ab494d99573ccbdad1
Arif Iqbal <arif_iqbal@yahoo.com>
2023-06-01 19:34:44 -0600

    modified:   script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ const slider = document.querySelector('main'),
   slides = Array.from(document.querySelectorAll('section'));
 console.log(slides);
 
-const endWidth = 25;
+const endWidth = 15;
 scrollContainer.addEventListener('wheel', (evt) => {
   // console.log(evt.target.window);
   console.log((scrollContainer.scrollLeft += evt.deltaY));
