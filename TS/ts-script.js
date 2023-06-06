aadf679ed25f16e9a807789d9cdf7d4e6022d5f7
Arif Iqbal <arif_iqbal@yahoo.com>
2023-06-05 18:08:56 -0600

    modified:   TS/ts-index.html modified:   TS/ts-script.js modified:   TS/ts-style.css modified:   style.css

--- TS/ts-script.js
+++ TS/ts-script.js
@@ -1,3 +1,84 @@
+/*****************/
+const scrollContainer = document.querySelector('main');
+/*****************/
+console.log(scrollContainer);
+
+const slider = document.querySelector('main'),
+  slides = Array.from(document.querySelectorAll('section'));
+console.log(slides);
+
+const endWidth = 15;
+scrollContainer.addEventListener('wheel', (evt) => {
+  // console.log(evt.target.window);
+  console.log((scrollContainer.scrollLeft += evt.deltaY));
+  /*****************/
+  evt.preventDefault();
+  // scrollContainer.scrollLeft += evt.deltaY;
+  /*****************/
+  let scrolled = (scrollContainer.scrollLeft += evt.deltaY);
+  console.log(scrollContainer.scrollLeft);
+  const sectionWidth = scrollContainer.scrollWidth;
+  const totalWidth = (scrollContainer.scrollLeft / sectionWidth) * 100;
+  console.log(totalWidth);
+  document.getElementById('myBar').style.width = totalWidth + endWidth + '%';
+});
+
+// scrollContainer.addEventListener('ontouchStart', (e) => {
+//   var x = e.touches[0].clientX;
+//   var y = e.touches[0].clientY;
+//   document.querySelectorAll('section').innerHTML = x + ', ' + y;
+// });
+
+// function myFunction(event) {
+//   var x = event.touches[0].clientX;
+//   var y = event.touches[0].clientY;
+//   document.querySelectorAll('section').innerHTML = x + ', ' + y;
+// }
+
+slides.forEach((slide, index) => {
+  console.log(slide, index);
+
+  // Touch Events
+  // slide.addEventListener('touchstart', touchStart(index));
+  // slide.addEventListener('touchend', touchEnd);
+  // slide.addEventListener('touchmove', touchMove);
+
+  // Mouse.events;
+  // slide.addEventListener('mousedown', touchStart(index));
+  // slide.addEventListener('mouseup', touchEnd);
+  // slide.addEventListener('mouseleave', touchEnd);
+  // slide.addEventListener('mousemove', touchMove);
+});
+
+/***********W3****************************************** 
+// When the user scrolls the page, execute myFunction
+window.onscroll = function () {
+  console.log('I am scrolled');
+  myFunction();
+};
+
+function myFunction() {
+  var winScroll = document.body.scrollTop || document.documentElement.scrollTop;
+  console.log(winScroll);
+  var height =
+  document.documentElement.scrollHeight -
+  document.documentElement.clientHeight;
+  console.log(height);
+  var scrolled = (winScroll / height) * 100;
+  console.log(scrolled);
+  document.getElementById('myBar').style.width = scrolled + '%';
+}
+/***********W3****************************************** */
+
+/*********************************************** */
+/*********************************************** */
+/*********************************************** */
+/*********************************************** */
+/*********************************************** */
+/*********************************************** */
+
+/*
+
 const slider = document.querySelector('.slider-container'),
   slides = Array.from(document.querySelectorAll('picture'));
 console.log(slides);
@@ -97,3 +178,5 @@ const setPositionByIndex = () => {
   prevTranslate = currentTranslate;
   setSliderPosition();
 };
+
+*/
